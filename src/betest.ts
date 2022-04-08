15cff7db8ad73be24e54b6eec091da509ac0be09
Anton Onyshchenko <antononyshch@gmail.com>
2022-04-08 21:00:26 +0300

    Add exports

--- src/betest.ts
+++ src/betest.ts
@@ -1,7 +1,7 @@
 import { BetestGroup } from "./b-group.js";
 import { BetestTestResult } from "./b-test-tesult.js";
 import { ResultEmiters } from "./results/results-emiters.js";
-import {ResultEmiter} from './results/result-emiter.js';
+import { ResultEmiter } from './results/result-emiter.js';
 import { TableResultEmiter } from "./results/table-emiter.js";
 import { LineResultEmiter } from "./results/line-emiter.js";
 
